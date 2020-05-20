9ef46955cc682180bb379226dc98922c01529a9d
Bernardo Luz <beluz2512@gmail.com>
2020-05-20 01:06:33 -0300

    subindo a aula sobte rest e spread ' '

--- main.js
+++ main.js
@@ -49,6 +49,43 @@ const usuario ={
 
     //==================== Rest/Spread ================
     
+    //rest
+    const usuario1 = {
+        nome:'bernardo',
+        idade: 19,
+        empresa:'Koerich'
+    };
+
+    const { nome1 ,...resto} = usuario1;
+    const arr3 = [1,2,3,4];
+    const [a,b, ...c] = arr3;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+
+    function soma(...params){
+        return params.reduce((total,next) => total + next);
+    }
+    console.log(soma(1,3,4));
+    
+
+    //spread
+    const arr4 = [1,2,3];
+    const arr5 = [4,5,6];
+
+    const arr6 = [...arr4,...arr5];
+
+    const usuario2 = {
+        nome2 :"Bernardo",
+        idade1:19,
+        empresa1:'Koerich'
+    };
+
+    const usuario3 = {...usuario2,nome:'Carlos'};
+    
+
+
+
 
 
 
